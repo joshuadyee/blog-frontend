d535bfefc1a360c023826cfd80553a1e8017c3c4
Joshua Yee <joshyee16@gmail.com>
2023-12-18 11:49:40 -0800

    install axios and use it to get data from api, reset posts to show data from response, use useEffect to call function

--- src/PostsNew.jsx
+++ src/PostsNew.jsx
@@ -3,12 +3,15 @@ export function PostsNew() {
     <div id="posts-new">
       <h1>New</h1>
       <h2>A Little Bit of Background</h2>
-      <body>
+      <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem vero tempora mollitia repudiandae ab sunt ex corporis dignissimos, inventore eveniet nisi iure dolore? Iste velit aut ipsa est autem aperiam.
         Ea, quibusdam illo repellendus expedita nisi aut at quas, commodi odit consequatur architecto nesciunt, temporibus iste voluptas natus velit possimus vel assumenda rem nam dolores? Quidem rerum nisi amet voluptate.
         Corporis voluptas nihil error neque earum iste vitae laborum, ea et rem tempore deleniti blanditiis reprehenderit illum, expedita, veritatis laboriosam mollitia quaerat porro optio quo dicta esse. Alias, natus in?
-      </body>
+      </p>
       <img src="https://images.unsplash.com/flagged/photo-1575555201693-7cd442b8023f?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bG9zJTIwYW5nZWxlc3xlbnwwfHwwfHx8MA%3D%3D" alt="LA background" />
+      <p>
+        <button>Make a new blog post here!</button>
+      </p>
     </div>
 
   );
