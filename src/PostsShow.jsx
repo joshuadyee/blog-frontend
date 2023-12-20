dc2735f41af4a6fdeca67d534cc403e63ead28ea
Joshua Yee <joshyee16@gmail.com>
2023-12-20 11:47:14 -0800

    basic edit/update on postShow and content pages

--- src/PostsShow.jsx
+++ src/PostsShow.jsx
@@ -1,9 +1,29 @@
+import axios from "axios"
+
 export function PostsShow(props) {
+
+  const updatePost = event => {
+    event.preventDefault()
+    const params = new FormData(event.target)
+    axios.patch(`http://localhost:3000/posts/${props.post.id}.json`).then(response => {
+      console.log(response.data)
+    })
+  }
+  
   return (
+    <>
     <div>
-      <p>Posts Show</p>
-      <p><b>Title: </b>{props.post.title}</p>
-      <p><b>Info: </b>{props.post.body}</p>
+        <h2>Post</h2>
+          <p><b>Title: </b>{props.post.title}</p>
+          <p><b>Info: </b>{props.post.body}</p>
     </div>
+      <form onSubmit={updatePost}>
+        <p>Title: <input type="text" name="title" defaultValue={props.post.title} /></p>
+        <p>Body: <input type="text" name="body" defaultValue={props.post.body} /></p>
+        <p>Image: <input type="text" name="image" defaultValue={props.post.image} /></p>
+        <p>User ID: <input type="text" name="user_id" defaultValue={props.post.user_id} /></p>
+        <button>Update Post</button>
+      </form>
+    </>
   )
 }
